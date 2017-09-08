d95d17f6627a136de3e9027a802e0015b332c8d3
Pavel Kretov <firegurafiku@gmail.com>
2017-09-08 19:09:57 +0300

    Add invalid JSON samples from James McLaughlin's project
    Their original code was found at json-parser project [1], BSD-licensed
    (two-clause), with the following copyright statement:
    
        Copyright (C) 2012, 2013 James McLaughlin et al.
        All rights reserved
    
    Since MIT and BSD licenses seem to be compatible, there are no legal
    issues arises due to borrowing this code.
    
    [1]: https://github.com/udp/json-parser/blob/master/LICENSE
    
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- test/test-json.sh
+++ test/test-json.sh
@@ -4,51 +4,24 @@ set -o nounset
 
 source src/json.sh
 
-echo "=== Test 1 ==="
-json:printProperties '{}'
+function test:json@testInvalidSample {
+    local filename="$1"
+    local contents="$(<"$filename")"
+    local err=0
+    json:printProperties "$contents" >/dev/null || err=$?
 
-echo "=== Test 2 ==="
-json:printProperties '{"test": "1"}'
+    if [[ "$err" -eq 41 ]]; then
+        return 0
+    fi
 
-echo "=== Test 2 ==="
-json:printProperties '{"test": {
-                         "a": "1",
-                         "b": "2" }}'
+    return 1
+}
 
-echo "=== Test 3 ==="
-json:printProperties '{
-  "public_key": "HQsmHLoeyBlJf8Eu1jlmzuU+ZaLkjPkgcvmokRUCIo8=",
-  "_embedded": {
-    "sort": "",
-    "path": "disk:/foo",
-    "items": [
-      {
-        "path": "disk:/foo/bar",
-        "type": "dir",
-        "name": "bar",
-        "modified": "2014-04-22T10:32:49+04:00",
-        "created": "2014-04-22T10:32:49+04:00"
-      },
-      {
-        "name": "photo.png",
-        "preview": "https://downloader.disk.yandex.ru/preview/...",
-        "created": "2014-04-21T14:57:13+04:00",
-        "modified": "2014-04-21T14:57:14+04:00",
-        "path": "disk:/foo/photo.png",
-        "md5": "4334dc6379c8f95ddf11b9508cfea271",
-        "type": "file",
-        "mime_type": "image/png",
-        "size": 34567
-      }
-    ],
-    "limit": 20,
-    "offset": 0
-  },
-  "name": "foo",
-  "created": "2014-04-21T14:54:42+04:00",
-  "custom_properties": {"foo":"1", "bar":"2"},
-  "public_url": "https://yadi.sk/d/2AEJCiNTZGiYX",
-  "modified": "2014-04-22T10:32:49+04:00",
-  "path": "disk:/foo",
-  "type": "dir"
-}'
+declare filename
+for filename in test/json-parser-samples/invalid-*.json; do
+    if test:json@testInvalidSample "$filename"; then
+        echo "PASS -- $filename"
+    else
+        echo "FAIL -- $filename"
+    fi
+done
